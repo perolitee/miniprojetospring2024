2eb07b359ec525dbcd49b0f4129d2818d5cb55a2
adrianodomingues <adrianodomingues@hotmail.com>
2024-11-08 23:03:16 -0300

    teste unitário

---
+++ src/test/java/br/com/itb/miniprojetospring/service/ProdutoServiceTest.java
@@ -0,0 +1,36 @@
+package br.com.itb.miniprojetospring.service;
+
+import br.com.itb.miniprojetospring.model.Produto;
+import br.com.itb.miniprojetospring.model.ProdutoRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ProdutoServiceTest {
+
+    @Mock
+    private ProdutoRepository produtoRepository;
+    @Mock
+    private ProdutoService produtoService;
+
+    @Test
+    public void findAllByIdTest(){
+
+        Produto produto = new Produto(1, "bolacha");
+        when(produtoRepository.findAllById(produto.getId())).thenReturn(produto);
+        Produto produtoRetornado = this.produtoService.findAllById(1);
+
+        assertEquals(produto, produtoRetornado);
+        verify(this.produtoRepository).findAllById(1);
+    }
+
+
+
+}
